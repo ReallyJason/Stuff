ba1a56b3da31239fdcea64d21267cb3c3763bc8c
ReallyJason <92177960+ReallyJason@users.noreply.github.com>
2023-10-11 10:57:17 -0400

    Update 010numberGuesser.java

--- 010numberGuesser.java
+++ 010numberGuesser.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 010numberGuesser.java{
+public class 010numberGuesser{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int diff=50, chance=50;
