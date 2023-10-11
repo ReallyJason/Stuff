98cc57dc6a96717bb1db932c85bd697980a7347f
ReallyJason <92177960+ReallyJason@users.noreply.github.com>
2023-10-11 11:00:10 -0400

    Update 010numberGuesser.java

--- 010numberGuesser.java
+++ 010numberGuesser.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 010numberGuesser{
+public class numberGuesser{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int diff=50, chance=50;
@@ -11,14 +11,9 @@ public class 010numberGuesser{
             System.out.printf("Is your number less than or equal to %d: ", chance);
             question=s.nextLine();
             System.out.println(diff);
-            if(question.equals("Yes")){
-                diff=diff/2-diff%2;
-                chance=chance-diff;
-            }
-            else{
-                diff=diff/2+diff%2;
-                chance=chance+diff;
-            }
+            if(question.equals("Yes"))diff=diff/2-diff%2;
+            else diff=diff/2+diff%2;
+            chance=chance+diff;
             if(diff/2==0)break;
             if(chance<1 || chance>100){
                 System.out.print("Your number is not in the range of 1-100");
@@ -28,3 +23,4 @@ public class 010numberGuesser{
         System.out.printf("Your number is %d", chance);
     }
 }
+
